98798c19fe55f7899c3e14cda908458552213838
DESKTOP-P4RVHFF\proconsul <simone.piccirilli@proconsul-gropu.com>
2022-10-04 18:31:45 +0200

    minor fix cahce

--- src/main/java/com/seor0/cache/fragment/service/SecSessionIsValid.java
+++ src/main/java/com/seor0/cache/fragment/service/SecSessionIsValid.java
@@ -13,6 +13,7 @@ public class SecSessionIsValid {
 	@Autowired
 	private SessionService ssServ;
 	
+	// ritorna booleano
 	public boolean SecuretyIsValid(String bt, String secSessionId) {
 		
 		SessionResponse sessionDto = ssServ.getSession(bt);
@@ -22,6 +23,21 @@ public class SecSessionIsValid {
 		
 		// controllo che id sicurezza combacino
 		if(!sessionDto.getSessionId().equals(secSessionId))
+			return false;
+		// tutto ok
+		return true;
+	}
+	
+	// controlla che sia attiva ed in l2
+	public boolean SecSessionIsValidL2(String bt) {
+		
+		SessionResponse sessionDto = ssServ.getSession(bt);
+		// controllo che la sessione esista, e sia attiva.
+		if(!ObjectUtils.isEmpty(sessionDto.getSessionNoExist()) && Boolean.FALSE.equals(sessionDto.isSessionActive()))
+			return false;
+		
+		// controllo che id sicurezza combacino
+		if(!sessionDto.getScope().equals("L2"))
 			return false;
 		// tutto ok
 		return true;
